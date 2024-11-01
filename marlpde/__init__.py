fe37ce8db39b454c04c11644be098b4b44d433f6
Hanno Spreeuw <h.spreeuw@esciencecenter.nl>
2024-11-01 14:43:36 +0100

    Following advice from @jhidding, this should be a
    better way to define a __init__.py

--- marlpde/__init__.py
+++ marlpde/__init__.py
@@ -1,2 +1,9 @@
-import pathlib, sys
+import pathlib
+import sys
+from .LHeureux_model import LMAHeureuxPorosityDiff
+from .parameters import Map_Scenario, Solver, Tracker
+
+# Control what pdoc will document, i.e. output to .html.
+__all__ = ['LMAHeureuxPorosityDiff', 'Map_Scenario', 'Solver', 'Tracker']
+
 sys.path.append(str(pathlib.Path(__file__).parent))
